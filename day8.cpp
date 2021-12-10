90c597ee2f286affa84aa4b1e4fdcce84468e9ed
Neil <neilwchen@gmail.com>
2021-12-10 00:31:56 -0500

    f

--- day8.cpp
+++ day8.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
 #include <sstream>
 #include <fstream>
+#include <deque>
 #include <vector>
 #include <algorithm>
 #include <string>
 #include <map>
+#include <utility>
+#include <iterator>
 using namespace std;
 
 struct Input {
     vector<vector<string>> patterns;
     vector<vector<string>> outputs;
 };
+
+template<typename T>
+void pv(const T &t) {
+    copy(t.begin(), t.end(), ostream_iterator<typename T::value_type>(cout, ", "));
+    cout << endl;
+}
 
 vector<string> stov(string &input) {
     vector<string> vec;
@@ -77,8 +86,141 @@ int partOne(vector<vector<string>> &digits) {
     return ntimes;
 }
 
+vector<vector<char>> mappings() {
+    vector<vector<char>> v;
+
+    // generate every ordering of the word abcdefg
+    // where
+    // a b c d e f g
+    // 0 1 2 3 4 5 6
+    // (ordering so far, remaining chars)
+    string abcdefg = "abcdefg";
+    vector<char> letters; //(abcdefg.begin(), abcdefg.end());
+    deque<char> remaining;
+    for (auto &c : abcdefg)
+        remaining.push_back(c);
+
+    deque<pair<vector<char>, deque<char>>> q = {make_pair(letters, remaining)};
+
+    while (!q.empty()) {
+        pair<vector<char>, deque<char>> p = q.front();
+        q.pop_front();
+
+        vector<char> sofar = p.first;
+        deque<char> remaining = p.second;
+
+        if (remaining.size() == 0)
+            v.push_back(sofar);
+
+        for (int i=0; i < remaining.size(); ++i) {
+            vector<char> _sofar = sofar;
+            _sofar.push_back(remaining[i]);
+            deque<char> _remaining = remaining;
+            _remaining.erase(_remaining.begin() + i);
+            pair<vector<char>, deque<char>> _p = make_pair(_sofar, _remaining);
+            q.push_back(_p);
+        }
+    }
+
+    return v;
+}
+
+bool isValidMapping(vector<char> &m, vector<string> patterns) {
+    map<vector<int>, int> segmentsToNum = {
+        {{0, 1, 2, 4, 5, 6}, 0},
+        {{2, 5}, 1},
+        {{0, 2, 3, 4, 6}, 2},
+        {{0, 2, 3, 5, 6}, 3},
+        {{1, 2, 3, 5}, 4},
+        {{0, 1, 3, 5, 6}, 5},
+        {{0, 1, 3, 4, 5, 6}, 6},
+        {{0, 2, 5}, 7},
+        {{0, 1, 2, 3, 4, 5, 6}, 8},
+        {{0, 1, 2, 3, 5, 6}, 9}
+    };
+
+    for (string &pattern : patterns) {
+        // e.g. 01236 -> 9
+        vector<int> segments;
+        for (char &c : pattern) { // const char &c?
+            vector<char>::iterator it = find(m.begin(), m.end(), c);
+            segments.push_back((int)(it - m.begin())); // TODO this find is not necessary
+        }
+        sort(segments.begin(), segments.end()); // TODO getting worse
+        if (segmentsToNum.count(segments) > 0) {
+            segmentsToNum.erase(segments);
+        } else {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+vector<int> toDigits(vector<char> &m, vector<string> strs) {
+    map<vector<int>, int> segmentsToNum = {
+        {{0, 1, 2, 4, 5, 6}, 0},
+        {{2, 5}, 1},
+        {{0, 2, 3, 4, 6}, 2},
+        {{0, 2, 3, 5, 6}, 3},
+        {{1, 2, 3, 5}, 4},
+        {{0, 1, 3, 5, 6}, 5},
+        {{0, 1, 3, 4, 5, 6}, 6},
+        {{0, 2, 5}, 7},
+        {{0, 1, 2, 3, 4, 5, 6}, 8},
+        {{0, 1, 2, 3, 5, 6}, 9}
+    };
+
+    vector<int> v;
+    for (auto str : strs) {
+        vector<int> segments;
+        for (char c : str) {
+            vector<char>::iterator it = find(m.begin(), m.end(), c);
+            segments.push_back((int)(it - m.begin()));
+        }
+        sort(segments.begin(), segments.end());
+        int digit = segmentsToNum[segments];
+        v.push_back(digit);
+    }
+    return v;
+}
+
+int partTwo(struct Input &input) {
+    // for each segment, keep list of candidate values it could be
+    // then eliminate invalid candidates for each segment
+    // until only candidate remains per segment?
+
+    auto ms = mappings();
+    int out = 0;
+
+    for (int i=0; i < input.patterns.size(); ++i) {
+        int cur = 0;
+        auto pattern = input.patterns[i];
+        auto output = input.outputs[i];
+        for (vector<char> m : ms) {
+            if (isValidMapping(m, pattern)) {
+                for (auto digit : toDigits(m, output)) {
+                    cur *= 10;
+                    cur += digit;
+                }
+                continue;
+            }
+        }
+        out += cur;
+    }
+    return out;
+}
+
 int main(int argc, char **argv) {
     Input input = vread(argv[1]);
-    cout << partOne(input.outputs) << endl;
+    // cout << partOne(input.outputs) << endl;
+
+    // vector<char> v = {'d','e','a','f','g','b','c'};
+    // vector<string> _v = {"cdfeb", "fcadb", "cdfeb", "cdbaf"};
+    // cout << isValidMapping(v, input.patterns[0]) << endl;
+    // pv(toDigits(v, _v));
+    // cout << endl;
+
+    cout << partTwo(input) << endl; 
     return 0;
 }
