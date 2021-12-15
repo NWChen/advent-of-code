be612a951003914dd5246fa16bfdf3a677e4d490
Neil <neilwchen@gmail.com>
2021-12-15 00:27:32 -0500

    day13

--- day13.cpp
+++ day13.cpp
@@ -6,9 +6,11 @@
 using namespace std;
 
 struct Point {
-    int x;
-    int y;
+   int x;
+   int y;
 };
+
+typedef vector<vector<bool>> Grid;
 
 struct Fold {
     string dim; 
@@ -16,24 +18,49 @@ struct Fold {
 };
 
 struct Input {
-    vector<Point> points;
+    shared_ptr<Grid> grid;
     vector<Fold> folds;
 };
 
+Point readPt(const string &line) {
+    int commaIndex = line.find(',');
+    string x = line.substr(0, commaIndex);
+    string y = line.substr(commaIndex+1, line.length());
+
+    return {
+        stoi(x),
+        stoi(y)
+    };
+}
+
 shared_ptr<Input> read(const string &filename) {
-    vector<Point> v;
-    vector<Fold> f;
+    // vector<Point> v;
     ifstream file;
     file.open(filename);
-    string line, x, y;
+    string line;
+
     struct Input input;
+    int xmax = 0, ymax = 0;
+
+    // find max x / y
+    while (getline(file, line) && line.size()) {
+        Point point = readPt(line);
+        xmax = max(xmax, point.x);
+        ymax = max(ymax, point.y);
+    }
+
+    Grid grid(
+        ++ymax,
+        vector<bool>(++xmax, false)
+    );
+    vector<Fold> f;
+    file.clear();
+    file.seekg(0);
 
     while (getline(file, line)) {
         if (line.size() && line[0] != 'f') {
-            int commaIndex = line.find(',');
-            x = line.substr(0, commaIndex);
-            y = line.substr(commaIndex+1, line.length());
-            v.push_back({stoi(x), stoi(y)});
+            Point pt = readPt(line);
+            grid[pt.y][pt.x] = true;
         } else {
             int eqIndex = line.find('=');
             if (eqIndex != string::npos) {
@@ -44,18 +71,77 @@ shared_ptr<Input> read(const string &filename) {
         }
     }
 
-    input.points = v;
+    input.grid = make_shared<Grid>(grid);
     input.folds = f;
     return make_shared<Input>(input);
 }
 
-int main(int argc, char **argv) {
-    shared_ptr<Input> input = read(argv[1]);
-    for (auto &pt : input->points) {
-        cout << pt.x << "," << pt.y << endl;
+void foldOverY(Grid &grid, int &y) {
+    int last_unfolded = y - 1;
+    for (Grid::iterator it = grid.begin() + y + 1; it < grid.end() && last_unfolded >= 0; ++it) {
+        for (int i=0; i < (*it).size(); ++i) {
+            bool f = (*it).at(i);
+            grid[last_unfolded][i] = grid[last_unfolded][i] | f;
+        }
+        --last_unfolded; 
     }
 
-    for (auto &fold : input->folds) {
-        cout << fold.dim << "=" << fold.value << endl;
+    // now truncate the rest of the grid
+    for (int i = grid.size(); i > y ; --i) {
+        grid.pop_back();        
     }
+}
+
+void foldOverX(Grid &grid, int &x) {
+    for (int offset = 1; offset < grid[0].size() && (x - offset) >= 0; ++offset) {
+        for (int y=0; y < grid.size(); ++y) {
+            grid[y][x - offset] = grid[y][x-offset] | grid[y][x+offset];
+        }
+    }
+
+    // now truncate the rest of the grid
+    int xmax = grid[0].size();
+    for (int y = 0; y < grid.size(); ++y) {
+        for (int i = xmax - 1; i >= x; --i) {
+            grid[y].pop_back();    
+        }
+    }
+}
+
+void pgrid(Grid &grid) {
+    for (auto &row : grid) {
+        for (auto e : row) {
+            if (e)
+                cout << "#";
+            else
+                cout << ".";
+        }
+        cout << endl;
+    }
+}
+
+int partOne(shared_ptr<Grid> grid, Fold fold) {
+    if (fold.dim == "x")
+        foldOverX(*grid, fold.value); // TODO this pass-by-reference of object of shared_ptr is strange, and maybe doesn't do what you think?
+    if (fold.dim == "y")
+        foldOverY(*grid, fold.value);
+
+    // int ymax = (*(input->grid)).size(), xmax = (*(input->grid)).at(0).size();
+    int out = 0;
+    for (auto &row : *grid) {
+        for (auto e : row) {
+            out += e;
+        }
+    }
+    return out;
+}
+
+int main(int argc, char **argv) {
+    shared_ptr<Input> input = read(argv[1]);
+
+    cout << partOne(input->grid, input->folds.at(0)) << endl;
+    // pgrid(*(input->grid));
+    // for (auto &fold : input->folds) {
+        // cout << fold.dim << "=" << fold.value << endl;
+    // }
 }
