c4ef6790f1a9b966efa04a457978f52e128461ab
NWChen <neilwchen@gmail.com>
2025-12-04 01:01:23 -0500

    3_2

--- 2025_py/3_2.py
+++ 2025_py/3_2.py
@@ -32,7 +32,19 @@ import sys
 
 def max_joltage(b: int) -> int:
     sb = str(b)
+    idxs = [(len(sb) - i) for i in range(1, 13)][::-1]
+    min_idx = -1
+    for i, idx in enumerate(idxs):
+        max_idx, max_digit = idx, 0
+        while idx > min_idx:
+            if int(sb[idx]) >= max_digit:
+                max_digit = int(sb[idx])
+                max_idx = idx
+            idx -= 1
+        idxs[i] = max_idx
+        min_idx = max(min_idx, max_idx)
 
+    out = int("".join(sb[idx] for idx in idxs))
     return out
 
 
