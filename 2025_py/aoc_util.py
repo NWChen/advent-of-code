44748e686dcbf488ad9a74a54009c52b1362760f
NWChen <neilwchen@gmail.com>
2025-12-06 18:09:24 -0500

    cleanup dir

--- 2025_py/aoc_util.py
+++ 2025_py/aoc_util.py
@@ -3,8 +3,8 @@ import sys
 
 def read(day: int, strip: bool = True):
     assert 0 < day <= 12
-    input_file = sys.argv[1] if len(sys.argv) > 1 else f"{day}.demo"
-    with open(input_file, "r") as f:
+    input_filepath = sys.argv[1] if len(sys.argv) > 1 else f"{day}.demo"
+    with open(input_filepath, "r") as f:
         out = f.read()
         return out.strip() if strip else out
     return []
