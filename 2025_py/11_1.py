c9f42d270b2021c26d1c5f83e4919e364616971a
NWChen <neilwchen@gmail.com>
2025-12-20 17:53:12 -0800

    add 11 rec

--- 2025_py/11_1.py
+++ 2025_py/11_1.py
@@ -5,15 +5,29 @@ adj = {
     for [k, v] in map(lambda line: line.split(": "), read(11).splitlines())
 }
 
-q = [("you", {"you"})]
-npaths = 0
-while q:
-    cur, visited = q.pop()
-    if cur == "out":
-        npaths += 1
-        continue
-    for neighbor in adj[cur]:
-        if neighbor not in visited:
-            q.append((neighbor, set(visited) | {neighbor}))
 
-print(npaths)
+def solve_iterative():
+    q = [("you", {"you"})]
+    npaths = 0
+    while q:
+        cur, visited = q.pop()
+        if cur == "out":
+            npaths += 1
+            continue
+        for neighbor in adj[cur]:
+            if neighbor not in visited:
+                q.append((neighbor, set(visited) | {neighbor}))
+    return npaths
+
+
+def solve_recursive():
+    def dfs(src: str, dst: str):
+        if src == dst:
+            return 1
+        paths = sum(dfs(v, dst) for v in adj[src])
+        return paths
+
+    return dfs("you", "out")
+
+
+print(solve_recursive())
