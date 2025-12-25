f1e5b497b30f7018d1e5b244e4d3e6321780e820
NWChen <neilwchen@gmail.com>
2025-12-25 14:13:51 -0500

    day 10 and easter eggs

--- 2025_py/10_2.py
+++ 2025_py/10_2.py
@@ -1,21 +1,21 @@
-from collections import deque
 from dataclasses import dataclass
+from itertools import product
 
+import numpy as np
 from aoc_util import read
+from scipy.optimize import linprog
 
 
 @dataclass
 class Machine:
-    lights: list[bool]
     buttons: list[tuple[int]]
     joltages: list[int]
 
 
 def parse(line: str):
     tokens = line.split(" ")
-    lights_str, buttons_list, joltages_str = tokens[0], tokens[1:-1], tokens[-1]
+    _, buttons_list, joltages_str = tokens[0], tokens[1:-1], tokens[-1]
     return Machine(
-        lights=[True if c == "#" else False for c in list(lights_str[1:-1])],
         buttons=[
             button if isinstance(button, tuple) else (button,)
             for button in list(map(eval, buttons_list))
@@ -25,38 +25,54 @@ def parse(line: str):
 
 
 def fewest_button_presses(machine: Machine) -> int:
-    q = deque(
-        [
-            (
-                [False for _ in machine.lights],  # indicator light sequence
-                [0 for _ in machine.joltages],  # joltage counters
-                0,  # button presses
-            )
-        ]
-    )
-    target_lights, target_joltages = machine.lights, machine.joltages
-    visited = set(str(q[0][0]))
-    while q:
-        lights, joltages, presses = q.popleft()
-        print(q)
-        if lights == target_lights and joltages == target_joltages:
-            return presses
-        for button in machine.buttons:
-            new_lights, new_joltages = lights[:], joltages[:]
-            for toggle in button:
-                new_lights[toggle] = not new_lights[toggle]
-                new_joltages[toggle] += 1
-            s = str(new_joltages)
-            if s not in visited:
-                visited.add(s)
-                q.append((new_lights, new_joltages, presses + 1))
-    return -1
+    # 0. define system of lin eq
+    M, N = len(machine.joltages), len(machine.buttons)
+    A = np.zeros((M, N), dtype=int)
+    B = np.zeros((M,), dtype=int)
+    for i, joltage in enumerate(machine.joltages):
+        B[i] = joltage
+        for j, button in enumerate(machine.buttons):
+            A[i][j] = i in button
 
+    # 1. find possible button press combinations by solving system of lin eq
+    out = sum(linprog(np.ones((N,)), A_eq=A, b_eq=B, integrality=1).x)
+    return int(out)
+    # m = max(np.max(A), np.max(B)) + 1
+    # min_presses = np.inf
+    # for coefs in product(*[range(0, m) for _ in range(N)]):
+    #     X = np.array(coefs).T
+    #     if np.allclose(A @ X, B):
+    #         min_presses = min(min_presses, sum(coefs))
+    # return min_presses
+
+
+# expressing problem as a system of linear equations:
+# [.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+#   x4 + x5 = 3
+#   x1 + x5 = 5
+#   x2 + x3 + x4 = 4
+#   x0 + x1 + x3 = 7
+#
+# [...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+#   x0 + x2 + x3 = 7
+#   x3 + x4 = 5
+#   x0 + x1 + x3 + x4 = 12
+#   x0 + x1 + x4 = 7
+#   x0 + x2 + x4 = 2
+#
+# [.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}
+#   x0 + x1 + x2 = 10
+#   x0 + x2 + x3 = 11
+#   x0 + x2 + x3 = 11
+#   x0 + x1 = 5
+#   x0 + x1 + x2 = 10
+#   x2 = 5
 
 inputs = [parse(line) for line in read(10).splitlines()]
-presses = 0
+out = 0
 for i, machine in enumerate(inputs):
     res = fewest_button_presses(machine)
-    print(f"Found result for machine {i + 1}/{len(inputs)}: {res}")
-    presses += res
-print(presses)
+    print(f"Machine {i + 1}/{len(inputs)}: {res} presses")
+    out += res
+
+print(out)
